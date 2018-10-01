d389f17f64a4b3f260dfb6c5f6e89f93cead24da
robingoots <robin.goots@dynapps.Be>
2018-10-01 10:18:07 +0200

    Add client action

--- awesome_tshirt/static/src/js/awesome_dashboard.js
+++ awesome_tshirt/static/src/js/awesome_dashboard.js
@@ -1,1 +1,10 @@
-console.log('hello world');
+odoo.define('awesome_tshirt.dashboard', function (require) {
+    "use strict";
+
+    return '';
+});
+
+var AbstractAction = require('web.AbstractAction');
+var ClientAction = AbstractAction();
+
+core.action_registry.add('awesome_tshirt.menu-action', ClientAction);
