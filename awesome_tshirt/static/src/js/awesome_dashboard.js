bd17af227a05874a880e1c99b7289dae7c554c40
robingoots <robin.goots@dynapps.Be>
2018-10-01 10:54:50 +0200

    Rename

--- awesome_tshirt/static/src/js/awesome_dashboard.js
+++ awesome_tshirt/static/src/js/awesome_dashboard.js
@@ -1,15 +1,32 @@
+var Widget = require('web.Widget');
+var AbstractAction = require('web.AbstractAction');
+
 odoo.define('awesome_tshirt.dashboard', function (require) {
     "use strict";
 
-    var AbstractAction = require('web.AbstractAction');
     var ClientAction = AbstractAction.extend({
         init: function() {
             console.log('Hello world!');
         },
     });
 
-    core.action_registry.add('menu-action', ClientAction);
+    core.action_registry.add('tshirt-dashboard', ClientAction);
 
     return '';
+});
+
+var Counter = Widget.extend({
+    template: 'some.template',
+    events: {
+        'click button': '_onClick',
+    },
+    init: function (parent, value) {
+        this._super(parent);
+        this.count = value;
+    },
+    _onClick: function () {
+        this.count++;
+        this.$('.val').text(this.count);
+    },
 });
 
