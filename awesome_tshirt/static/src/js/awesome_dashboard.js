ad12bf23c39dbca66ea466daff83dcce2612cd84
robingoots <robin.goots@dynapps.Be>
2018-10-01 10:37:58 +0200

    Changes

--- awesome_tshirt/static/src/js/awesome_dashboard.js
+++ awesome_tshirt/static/src/js/awesome_dashboard.js
@@ -1,10 +1,11 @@
 odoo.define('awesome_tshirt.dashboard', function (require) {
     "use strict";
 
+    var AbstractAction = require('web.AbstractAction');
+    var ClientAction = AbstractAction();
+
+    core.action_registry.add('awesome_tshirt.menu-action', ClientAction);
+
     return '';
 });
 
-var AbstractAction = require('web.AbstractAction');
-var ClientAction = AbstractAction();
-
-core.action_registry.add('awesome_tshirt.menu-action', ClientAction);
