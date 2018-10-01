07ecb2c45a1b4dedb740936d79b88c30c54d10aa
robingoots <robin.goots@dynapps.Be>
2018-10-01 11:07:16 +0200

    Move

--- awesome_tshirt/static/src/js/awesome_dashboard.js
+++ awesome_tshirt/static/src/js/awesome_dashboard.js
@@ -1,9 +1,10 @@
-var Widget = require('web.Widget');
-var AbstractAction = require('web.AbstractAction');
+
+
 
 odoo.define('awesome_tshirt.dashboard', function (require) {
     "use strict";
 
+    var AbstractAction = require('web.AbstractAction');
     var ClientAction = AbstractAction.extend({
         init: function() {
             console.log('Hello world!');
@@ -11,6 +12,8 @@ odoo.define('awesome_tshirt.dashboard', function (require) {
     });
 
     core.action_registry.add('tshirt-dashboard', ClientAction);
+
+    var Widget = require('web.Widget');
 
     return '';
 });
